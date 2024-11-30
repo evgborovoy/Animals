e4c104b24f3ee1290249fb6b363b824722e1779c
Evgeniy <borovoyevgen@gmail.com>
2024-11-30 14:04:34 +0400

    Initial Commit

---
+++ Animals/AnimalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalsApp.swift
+//  Animals
+//
+//  Created by Evgeniy Borovoy on 11/30/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
